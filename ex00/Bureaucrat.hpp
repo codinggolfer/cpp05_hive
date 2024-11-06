cc6bb2c70b23a6f7ab8d8beb78b261c8f479d6e2
Eromon Agbomeirele <eagbomei@c2r2p5.hive.fi>
2024-11-06 12:33:55 +0200

    ready ex00

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+private:
+	const std::string _name;
+	int _grade;
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string name, int grade);
+	Bureaucrat(Bureaucrat& copy);
+	Bureaucrat& operator=(Bureaucrat& other);
+
+//member functions: getname and grade, decrement and increment.
+	std::string getName();
+	int getGrade();
+	void increamentGrade();
+	void decrementGrade();
+//member functions: getname and grade, decrement and increment.
+
+
+//Exception classes	
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char* what() const noexcept override;
+	};
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char* what() const noexcept override;
+	};
+//Exception classes	
+
+	~Bureaucrat();
+};
+
+std::ostream& operator<<(std::ostream& out, Bureaucrat byro);
